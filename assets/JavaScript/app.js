7fc6c43ef65df4f708b8b579001441eabaa2dd9f
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-07-30 16:20:54 +0200

    Resolve ts warnings

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -3,15 +3,16 @@ import "../Css/app.css";
 
 // Lit components
 
-import "../../packages/site-distribution/Resources/Private/Components/header/header.ts";
-import "../../packages/site-distribution/Resources/Private/Components/nav/nav.ts";
-import "../../packages/site-distribution/Resources/Private/Components/nav-item/nav-item.ts";
+import "../../packages/site-distribution/Resources/Private/Components/header/header";
+import "../../packages/site-distribution/Resources/Private/Components/nav/nav";
+import "../../packages/site-distribution/Resources/Private/Components/nav-item/nav-item";
 
 // Use vite-svg-loader, see https://www.npmjs.com/package/vite-svg-loader#user-content-import-params
 // import typo3Logo from '../Image/Icon/typo3.svg?url'
 // document.querySelector('.js-made-with').innerHTML = 'Made with <img width="20px" height="20px" src="' + typo3Logo + '" alt="TYPO3">'
 
 // TYPO3.lang contains all available inline labels
+// @ts-ignore
 console.log("Say hi! " + TYPO3.lang["site-distribution.welcome"]);
 
 // Shoelace components
